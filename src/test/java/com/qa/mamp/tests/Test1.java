67a7680a64f80314eb9ddb21d71a24b4aac8d85d
bluestar@57 <nagaraj8439@gmail.com>
2018-11-30 00:12:53 +0530

    New project added terminal

---
+++ src/test/java/com/qa/mamp/tests/Test1.java
@@ -0,0 +1,31 @@
+package com.qa.mamp.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("Test1");
+	}
+
+	@Test
+	public void m2()
+	{
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void m3()
+	{
+		System.out.println("Test3");
+	}
+	
+	@Test
+	public void m4()
+	{
+		System.out.println("Test4");
+	}
+}
---
+++ src/test/java/com/qa/mamp/tests/Test2.java
@@ -0,0 +1,32 @@
+package com.qa.mamp.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test2 
+{
+	@Test
+	public void m5()
+	{
+		System.out.println("Test5");
+	}
+
+	@Test
+	public void m6()
+	{
+		System.out.println("Test6");
+	}
+	
+	@Test
+	public void m7()
+	{
+		System.out.println("Test7");
+	}
+	
+	@Test
+	public void m8()
+	{
+		System.out.println("Test8");
+	}
+
+}
