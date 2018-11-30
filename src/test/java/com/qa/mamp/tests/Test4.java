d1dd83dccecc0965fb11952d47ef63ea578a5bb2
bluestar@57 <nagaraj8439@gmail.com>
2018-11-30 08:39:18 +0530

    Test4

---
+++ src/test/java/com/qa/mamp/tests/Test4.java
@@ -0,0 +1,32 @@
+package com.qa.mamp.tests;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+import org.testng.annotations.Test;
+
+public class Test4 
+{
+	@Test
+	public void m1()
+	{
+		System.out.println("Test1sssss");
+	}
+
+	@Test
+	public void m2()
+	{
+		System.out.println("Test2");
+	}
+	
+	@Test
+	public void m3()
+	{
+		System.out.println("Test3");
+	}
+	
+	@Test
+	public void m4()
+	{
+		System.out.println("Test4");
+	}
+}
